bbf8586e18d3b50ab0ba57925b56cbfd00695d31
mattia.rollo <ste.tia@hotmail.it>
2023-06-25 00:15:05 +0200

    add flask_login pk + auth logic

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app,db
 from flask import render_template, redirect, url_for, flash
 from app.models import Item, User
-from app.forms import RegisterForm
+from app.forms import RegisterForm, LoginForm
+from flask_login import login_user
 
 name='Mattia'
 surname = 'Rollo'
@@ -36,4 +37,17 @@ def register_page():
 
 @app.route('/login', methods=['GET','POST'])
 def login_page():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+            ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success') 
+            return redirect(url_for('market'))
+        else:
+            flash(f'Username and password are not match! Please try again', category='danger')
+
+            
+    return render_template('login.html', form=form)
