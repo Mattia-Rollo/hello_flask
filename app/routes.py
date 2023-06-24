bb145662284470bd4ecf6ee761e06bd8ba92d0d4
mattia.rollo <ste.tia@hotmail.it>
2023-06-24 22:19:34 +0200

    add validators wtforms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from app import app
-from flask import render_template
-from app.models import Item
+from app import app,db
+from flask import render_template, redirect, url_for, flash
+from app.models import Item, User
 from app.forms import RegisterForm
 
 name='Mattia'
@@ -18,7 +18,17 @@ def market():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address= form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market'))
+    if form.errors != {}: #if there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'there was an error with creating a user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
